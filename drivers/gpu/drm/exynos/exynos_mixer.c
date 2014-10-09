65a4bf2724917df1f71a55cd2c0aa8823c475a92
Daniel Drake <drake@endlessm.com>
2014-10-09 13:56:30 -0600

    drm/exynos/hdmi: add 85.5MHz pixel clock for v14 HDMI PHY
    Configuration details from Samsung. This enables 1366x768@60Hz,
    which also needs the 257px timing hack to work around a mixer
    limitation.
    
    Signed-off-by: Daniel Drake <drake@endlessm.com>

--- drivers/gpu/drm/exynos/exynos_mixer.c
+++ drivers/gpu/drm/exynos/exynos_mixer.c
@@ -1156,6 +1156,7 @@ static int mixer_atomic_check(struct exynos_drm_crtc *crtc,
 
 	/* Check against some specific resolutions. */
 	if ((w == 1024 && h == 768) ||
+		(w == 1366 && h == 768) ||
 		(w == 1280 && h == 1024))
 		return 0;
 
