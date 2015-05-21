0b7eb9eff2022aa9ca2c7794afdb19c10dfca7d8
Tobias Jakobi <tjakobi@math.uni-bielefeld.de>
2015-05-21 15:06:13 +0200

    drm/exynos: make fbdev support optional
    Similar to i915/msm we want to enable the user to
    build a kernel without fbdev support.

--- drivers/gpu/drm/exynos/exynos_drm_fb.c
+++ drivers/gpu/drm/exynos/exynos_drm_fb.c
@@ -208,6 +208,7 @@ struct exynos_drm_gem *exynos_drm_fb_gem(struct drm_framebuffer *fb, int index)
 
 static void exynos_drm_output_poll_changed(struct drm_device *dev)
 {
+#ifdef CONFIG_DRM_EXYNOS_FBDEV
 	struct exynos_drm_private *private = dev->dev_private;
 	struct drm_fb_helper *fb_helper = private->fb_helper;
 
@@ -215,6 +216,7 @@ static void exynos_drm_output_poll_changed(struct drm_device *dev)
 		drm_fb_helper_hotplug_event(fb_helper);
 	else
 		exynos_drm_fbdev_init(dev);
+#endif
 }
 
 static const struct drm_mode_config_funcs exynos_drm_mode_config_funcs = {
