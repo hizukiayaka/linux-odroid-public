e489cf42698666fcb4d4f56d9a428227f5d1cc66
Marek Szyprowski <m.szyprowski@samsung.com>
2015-11-10 14:23:26 +0100

    drm/exynos: move dma_addr attribute from exynos plane to exynos fb
    DMA address is a framebuffer attribute and the right place for it is
    exynos_drm_framebuffer not exynos_drm_plane. This patch also introduces
    helper function for getting dma address of the given framebuffer.
    
    Signed-off-by: Marek Szyprowski <m.szyprowski@samsung.com>
    Reviewed-by: Gustavo Padovan <gustavo.padovan@collabora.co.uk>

--- drivers/gpu/drm/exynos/exynos_drm_drv.h
+++ drivers/gpu/drm/exynos/exynos_drm_drv.h
@@ -56,8 +56,6 @@ extern void exynos4412_qos(u8 tm, u8 ac);
  * @crtc_h: window height to be displayed (hardware screen).
  * @h_ratio: horizontal scaling ratio, 16.16 fixed point
  * @v_ratio: vertical scaling ratio, 16.16 fixed point
- * @dma_addr: array of bus(accessed by dma) address to the memory region
- *	      allocated for a overlay.
  * @zpos: order of overlay layer(z position).
  *
  * this structure is common to exynos SoC and its contents would be copied
@@ -76,7 +74,6 @@ struct exynos_drm_plane {
 	unsigned int crtc_h;
 	unsigned int h_ratio;
 	unsigned int v_ratio;
-	dma_addr_t dma_addr[MAX_FB_BUFFER];
 	unsigned int zpos;
 	struct drm_framebuffer *pending_fb;
 };
