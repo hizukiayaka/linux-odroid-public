772f17bdfe07acdd7d4efdf70a7e52f47ef7c373
Marek Szyprowski <m.szyprowski@samsung.com>
2016-01-27 15:44:42 +0100

    drm: add generic blending related properties
    This patch adds code and documentation for the following blending
    related properties: 'alpha', 'blending' and 'alpha_premult'.
    
    'alpha' property defines plane's transparency used for some blending
    modes.
    
    'alpha_premult' property defines if RGB pixel data in the framebuffer
    contains values pre-multiplied by its alpha value or full range RGB
    values (0-255), which needs to be multiplied by pixel alpha value for
    blending operation.
    
    'blending' property selects blending mode - by selecting one of the
    predefined values, user can select a pair of factors, which will be
    used in the generic blending equation. For more information, see
    the kerneldoc for the added code.
    
    Signed-off-by: Marek Szyprowski <m.szyprowski@samsung.com>

--- include/drm/drm_crtc.h
+++ include/drm/drm_crtc.h
@@ -1277,6 +1277,11 @@ struct drm_plane_state {
 	unsigned int zpos;
 	unsigned int normalized_zpos;
 
+	/* Plane blending */
+	unsigned int blending;
+	unsigned int alpha;
+	bool alpha_premult;
+
 	struct drm_atomic_state *state;
 };
 
@@ -2100,6 +2105,9 @@ struct drm_mode_config {
 	struct drm_property *rotation_property;
 	struct drm_property *zpos_property;
 	struct drm_property *zpos_immutable_property;
+	struct drm_property *blending_property;
+	struct drm_property *alpha_property;
+	struct drm_property *alpha_premult_property;
 	struct drm_property *prop_src_x;
 	struct drm_property *prop_src_y;
 	struct drm_property *prop_src_w;
@@ -2510,6 +2518,13 @@ extern int drm_mode_create_zpos_property(struct drm_device *dev,
 extern int drm_mode_create_zpos_immutable_property(struct drm_device *dev,
 						   unsigned int min,
 						   unsigned int max);
+
+extern int drm_mode_create_blending_property(struct drm_device *dev,
+				      unsigned int *supported_blendings,
+				      unsigned int supported_blendings_count);
+extern int drm_mode_create_alpha_property(struct drm_device *dev,
+					  unsigned int max);
+extern int drm_mode_create_alpha_premult_property(struct drm_device *dev);
 
 /* Helpers */
 
